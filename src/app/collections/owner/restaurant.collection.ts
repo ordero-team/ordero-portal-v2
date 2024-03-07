f89ba2bef37af34047ab537ca749ba49aa1f635d
akbarsaputrait <akbarsaputrait@outlook.com>
2024-03-07 23:07:53 +0700

    Manage Restaurant

--- src/app/collections/owner/restaurant.collection.ts
+++ src/app/collections/owner/restaurant.collection.ts
@@ -4,21 +4,23 @@ import { MetalCollection, MetalCollectionConfig } from '@lib/metal-data';
 import { MetalAPIData } from '@mtl/interfaces';
 import { OwnerProfile } from './profile.collection';
 
-export interface Restaurant extends MetalAPIData {
+export interface OwnerRestaurant extends MetalAPIData {
   name: string;
   status: 'active' | 'inactive';
   slug: string;
+  phone: string;
+  website: string;
 
   owner?: OwnerProfile;
 }
 
-const RestaurantConfig: MetalCollectionConfig<Restaurant> = {
+const RestaurantConfig: MetalCollectionConfig<OwnerRestaurant> = {
   name: 'owner.restaurant',
   endpoint: 'restaurants',
 };
 
 @Injectable({ providedIn: 'root' })
-export class OwnerRestaurantCollection extends MetalCollection<Restaurant, OwnerOriginService> {
+export class OwnerRestaurantCollection extends MetalCollection<OwnerRestaurant, OwnerOriginService> {
   constructor(public origin: OwnerOriginService) {
     super(origin, RestaurantConfig);
   }
--- src/app/pages/restaurant/main/setting/restaurant/restaurant.component.ts
+++ src/app/pages/restaurant/main/setting/restaurant/restaurant.component.ts
@@ -1,16 +1,122 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { OwnerRestaurant, OwnerRestaurantCollection } from '@app/collections/owner/restaurant.collection';
 import { appIcons } from '@app/core/helpers/icon.helper';
 import { INavRoute } from '@app/core/services/navigation.service';
+import { OwnerAuthService } from '@app/core/services/owner/auth.service';
+import { ToastService } from '@app/core/services/toast.service';
+import { OwnerState } from '@app/core/states/owner/owner.state';
+import { DialogConfirmComponent } from '@app/shared/components/dialog-confirm/dialog-confirm.component';
+import { Form, FormRecord } from '@lib/form';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import { Select } from '@ngxs/store';
+import { Observable } from 'rxjs';
 
+@UntilDestroy()
 @Component({
   selector: 'aka-setting-restaurant',
   templateUrl: './restaurant.component.html',
   styleUrls: ['./restaurant.component.scss'],
 })
 export class SettingRestaurantComponent implements OnInit {
-  constructor() {}
+  @Form({
+    name: 'required|alphaNumSpace',
+    description: '',
+    website: '',
+    phone: '',
+  })
+  formData: FormRecord;
+  record: OwnerRestaurant;
 
-  ngOnInit(): void {}
+  image: File | null;
+  imageTemp: File | null;
+
+  @Select(OwnerState.currentRestaurant) currentResstaurant$: Observable<OwnerRestaurant>;
+
+  constructor(
+    private collection: OwnerRestaurantCollection,
+    private toast: ToastService,
+    private auth: OwnerAuthService,
+    private dialog: MatDialog
+  ) {
+    this.currentResstaurant$.pipe(untilDestroyed(this)).subscribe((user) => {
+      if (user) {
+        this.record = user;
+      }
+    });
+  }
+
+  ngOnInit() {
+    // if (this.record.avatar?.original) {
+    //   this.image = this.record.avatar.original;
+    //   this.imageTemp = this.image;
+    // }
+
+    this.applyData();
+  }
+
+  applyData() {
+    this.formData.$import({ ...this.record });
+  }
+
+  setImage(event) {
+    this.image = event;
+  }
+
+  removeImage() {
+    if (typeof this.image === 'string') {
+      const delRef = this.dialog.open(DialogConfirmComponent, {
+        data: {
+          title: `Delete Avatar`,
+          message: `Are you sure?`,
+          showConfirm: true,
+          confirmLabel: 'Delete',
+          confirmColor: 'warn',
+          showWarning: false,
+          class: 'min-w-60',
+        },
+      });
+
+      delRef.afterClosed().subscribe(async (result) => {
+        if (result) {
+          try {
+            // await this.collection.deleteAvatar();
+            // this.toast.info('Profile Picture deleted successfully!');
+            // this.image = null;
+            // this.imageTemp = null;
+            // await this.auth.fetchMe();
+            this.applyData();
+          } catch (error) {
+            this.toast.error({ title: 'Error', detail: 'Something went wrong' }, error);
+          }
+        }
+      });
+    } else {
+      this.image = null;
+      this.imageTemp = null;
+    }
+  }
+
+  async submit() {
+    this.formData.$loading = true;
+    try {
+      await this.collection.update('', this.formData.$payload);
+
+      // if (this.imageTemp !== this.image && this.image !== null) {
+      //   const resImg = await this.collection.updateAvatar(this.image);
+      //   await this.auth.fetchMe();
+      //   this.applyData();
+      //   this.image = this.imageTemp = resImg.avatar.original;
+      // }
+
+      await this.auth.fetchMe();
+      this.applyData();
+      this.toast.info('Personal Info updated successfully');
+    } catch (error) {
+      this.toast.error({ title: 'Error', detail: 'Something went wrong' }, error);
+    }
+    this.formData.$loading = false;
+  }
 }
 
 export const RestaurantSettingRestaurantNavRoute: INavRoute = {
